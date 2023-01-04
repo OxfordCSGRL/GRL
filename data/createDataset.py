10ef7d85b90eb56a571223ff78fc45ee7d47a77a
OxfordCSGRL <121568210+OxfordCSGRL@users.noreply.github.com>
2023-01-04 15:59:41 +0000

    Create createDataset.py

---
+++ data/createDataset.py
@@ -0,0 +1,13 @@
+
+
+
+import torch
+import os
+import typing
+import torch_geometric
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric.datasets as datasets
+
+from torch_geometric.nn import GCNConv
